db994ac82e41501e9e481ea2f87f001a24efb972
Davo2009 <96415581+Davo2009@users.noreply.github.com>
2026-09-20 23:36:15 +0100

    Add LED blinking pattern in blinker.ino
    This code controls multiple LEDs connected to pins 8, 9, 11, and 12, creating a blinking pattern with varying delays.

---
+++ blinker.ino
@@ -0,0 +1,93 @@
+/ C++ code
+//
+void setup()
+{
+  pinMode(8, OUTPUT);
+   pinMode(9, OUTPUT);
+   pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  delay (500);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  delay (1000);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  delay (1000);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  delay (1000);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  delay (1000);
+digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  delay (1000);
+digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  delay (1000);
+digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  delay (1000);
+digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay (1000);
+digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay (1000);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay (1000);
+digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay (1000);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  delay (1000);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  delay (1000);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  delay (1000);
+  
+
+
+
+  
+}
